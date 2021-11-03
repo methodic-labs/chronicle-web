fff6742840d61c62b3e6a704519b5293e876fefe
Solomon Tang <solomoncztang@gmail.com>
2021-11-02 19:43:04 -0700

    generateTableData function

---
+++ src/containers/dashboard/utils/generateTableData.js
@@ -0,0 +1,24 @@
+import padStart from 'lodash/padStart';
+import random from 'lodash/random';
+// @flow
+import range from 'lodash/range';
+import { DateTime } from 'luxon';
+import { v4 as uuid } from 'uuid';
+
+function generateTableData(numRows :number) :any[] {
+  return range(numRows)
+    .map((d, i) => {
+      const activeParticipants = random(25, 250);
+      const totalParticipants = random(activeParticipants, activeParticipants * 2);
+      return {
+        organization: `Organization ${padStart(i.toString(), 3, '0')}`,
+        studyId: uuid(),
+        dateLaunched: DateTime
+          .local().minus({ days: numRows - i }).toISO(),
+        activeParticipants,
+        totalParticipants,
+      };
+    });
+}
+
+export default generateTableData;
