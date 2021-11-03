5e5051edea18036fd23aaa310c954cd32735b489
Solomon Tang <solomoncztang@gmail.com>
2021-11-03 15:16:55 -0700

    dashboard has navlinks back to core app

--- src/containers/dashboard/DashboardContainer.js
+++ src/containers/dashboard/DashboardContainer.js
@@ -1,21 +1,31 @@
 // @flow
+import { AuthUtils } from 'lattice-auth';
 import {
   AppContainerWrapper,
   AppContentWrapper,
   AppHeaderWrapper,
+  AppNavigationWrapper,
   CardStack,
   Typography,
 } from 'lattice-ui-kit';
+import { NavLink } from 'react-router-dom';
 
 import StudiesTable from './components/StudiesTable';
 import SummaryLinePlot from './components/SummaryLinePlot';
 import SummaryStats from './components/SummaryStats';
 
 import OpenLatticeIcon from '../../assets/images/ol_icon.png';
+import { DASHBOARD, ROOT, STUDIES } from '../../core/router/Routes';
 
 const DashboardContainer = () => (
   <AppContainerWrapper>
-    <AppHeaderWrapper appIcon={OpenLatticeIcon} appTitle="Chronicle" />
+    <AppHeaderWrapper appIcon={OpenLatticeIcon} appTitle="Chronicle">
+      <AppNavigationWrapper>
+        <NavLink to={ROOT} />
+        <NavLink to={STUDIES}> Studies </NavLink>
+        { AuthUtils.isAdmin() && <NavLink to={DASHBOARD}>Dashboard</NavLink>}
+      </AppNavigationWrapper>
+    </AppHeaderWrapper>
     <AppContentWrapper>
       <CardStack>
         <Typography variant="h1">Global Dashboard</Typography>
