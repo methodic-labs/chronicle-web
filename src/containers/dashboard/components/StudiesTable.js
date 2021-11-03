b173faee2e4bfb54306aa713ace37a9101a137ac
Solomon Tang <solomoncztang@gmail.com>
2021-11-02 19:51:14 -0700

    style linting

--- src/containers/dashboard/components/StudiesTable.js
+++ src/containers/dashboard/components/StudiesTable.js
@@ -20,11 +20,13 @@ const StyledTableCard = styled(Card)`
     height: 520px;
     margin-bottom: 4px;
   }
+
   table {
     border-collapse: separate;
     border-spacing: 0;
     margin-bottom: 8px;
     min-width: 958px;
+
     ::after {
       content: "";
       position: absolute;
@@ -32,31 +34,34 @@ const StyledTableCard = styled(Card)`
       bottom: 1px;
       left: 1px;
       pointer-events: none;
-      background-image: linear-gradient(to bottom, 
-                        rgba(255,255,255, 0), 
-                        rgba(255,255,255, 1) 80%);
+      background-image: linear-gradient(to bottom, rgba(255 255 255 / 0%), rgba(255 255 255 / 100%) 80%);
       width: calc(100% - 2px);
       height: 1em;
       border-radius: 4px;
     }
+
     th {
       background-color: inherit;
       border: none;
       border-bottom: 1px solid black;
       font-size: 14px;
     }
+
     tr > :first-child {
       padding-left: 16px;
       overflow: hidden;
       text-overflow: ellipsis;
       white-space: nowrap;
     }
+
     tr > :last-child {
       padding-right: 16px;
     }
+
     tr > :nth-last-child(-n+2) {
       text-align: end;
     }
+
     td {
       border-bottom: 1px solid ${NEUTRAL.N100};
     }
