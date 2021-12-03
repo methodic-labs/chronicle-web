929a529ac17c8f4f001fef08360b78b3b4906c30
Solomon Tang <solomoncztang@gmail.com>
2021-12-02 21:33:33 -0800

    update studies table columns

--- src/containers/dashboard/components/constants.js
+++ src/containers/dashboard/components/constants.js
@@ -2,13 +2,14 @@ import dateTimeComparator from '../utils/dateTimeComparator';
 
 const STUDIES_TABLE_HEADERS = [
   { key: 'organization', label: 'Organization' },
+  { key: 'studyName', label: 'Study Name' },
   { key: 'studyId', label: 'Study ID', cellStyle: { width: '39ch' } },
-  {
-    key: 'dateLaunched',
-    label: 'Date Launched',
-    cellStyle: { width: '148px' },
-    comparator: dateTimeComparator
-  },
+  // {
+  //   key: 'dateLaunched',
+  //   label: 'Date Launched',
+  //   cellStyle: { width: '148px' },
+  //   comparator: dateTimeComparator
+  // },
   { key: 'activeParticipants', label: 'Active Part.', cellStyle: { width: '120px' } },
   { key: 'totalParticipants', label: 'Total Part.', cellStyle: { width: '120px' } },
 ];
