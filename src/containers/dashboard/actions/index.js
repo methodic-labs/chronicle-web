e54c7e77d35d1702ca5421ac43f44100294f76e0
Solomon Tang <solomoncztang@gmail.com>
2021-12-02 21:34:07 -0800

    add dashboard actions

--- src/containers/dashboard/actions/index.js
+++ src/containers/dashboard/actions/index.js
@@ -6,7 +6,22 @@ import type { RequestSequence } from 'redux-reqseq';
 const GET_ALL_STUDIES_TABLE_DATA :'GET_ALL_STUDIES_TABLE_DATA' = 'GET_ALL_STUDIES_TABLE_DATA';
 const getAllStudiesTableData :RequestSequence = newRequestSequence(GET_ALL_STUDIES_TABLE_DATA);
 
+const GET_ORG_STUDIES_TABLE_DATA :'GET_ORG_STUDIES_TABLE_DATA' = 'GET_ORG_STUDIES_TABLE_DATA';
+const getOrgStudiesTableData :RequestSequence = newRequestSequence(GET_ORG_STUDIES_TABLE_DATA);
+
+const GET_ORG_STUDIES :'GET_ORG_STUDIES' = 'GET_ORG_STUDIES';
+const getOrgStudies :RequestSequence = newRequestSequence(GET_ORG_STUDIES);
+
+const GET_STUDY_PARTICIPANTS_COUNT :'GET_STUDY_PARTICIPANTS_COUNT' = 'GET_STUDY_PARTICIPANTS_COUNT';
+const getStudyParticipantsCount :RequestSequence = newRequestSequence(GET_STUDY_PARTICIPANTS_COUNT);
+
 export {
   GET_ALL_STUDIES_TABLE_DATA,
+  GET_ORG_STUDIES,
+  GET_ORG_STUDIES_TABLE_DATA,
+  GET_STUDY_PARTICIPANTS_COUNT,
   getAllStudiesTableData,
+  getOrgStudies,
+  getOrgStudiesTableData,
+  getStudyParticipantsCount,
 };
