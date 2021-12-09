b3c169fbfa95b0cea8767bc3aacbbd59c71ac93f
Solomon Tang <solomoncztang@gmail.com>
2021-12-08 19:48:55 -0800

    summary stat actions

--- src/containers/dashboard/actions/index.js
+++ src/containers/dashboard/actions/index.js
@@ -15,13 +15,28 @@ const getOrgStudies :RequestSequence = newRequestSequence(GET_ORG_STUDIES);
 const GET_STUDY_PARTICIPANTS_COUNT :'GET_STUDY_PARTICIPANTS_COUNT' = 'GET_STUDY_PARTICIPANTS_COUNT';
 const getStudyParticipantsCount :RequestSequence = newRequestSequence(GET_STUDY_PARTICIPANTS_COUNT);
 
+const COUNT_ALL_STUDIES :'COUNT_ALL_STUDIES' = 'COUNT_ALL_STUDIES';
+const countAllStudies :RequestSequence = newRequestSequence(COUNT_ALL_STUDIES);
+
+const COUNT_ALL_PARTICIPANTS :'COUNT_ALL_PARTICIPANTS' = 'COUNT_ALL_PARTICIPANTS';
+const countAllParticipants :RequestSequence = newRequestSequence(COUNT_ALL_PARTICIPANTS);
+
+const GET_SUMMARY_STATS :'GET_SUMMARY_STATS' = 'GET_SUMMARY_STATS';
+const getSummaryStats :RequestSequence = newRequestSequence(GET_SUMMARY_STATS);
+
 export {
+  COUNT_ALL_PARTICIPANTS,
+  COUNT_ALL_STUDIES,
   GET_ALL_STUDIES_TABLE_DATA,
   GET_ORG_STUDIES,
   GET_ORG_STUDIES_TABLE_DATA,
   GET_STUDY_PARTICIPANTS_COUNT,
+  GET_SUMMARY_STATS,
+  countAllParticipants,
+  countAllStudies,
   getAllStudiesTableData,
   getOrgStudies,
   getOrgStudiesTableData,
   getStudyParticipantsCount,
+  getSummaryStats,
 };
