c3b9fc53a4c8d2b72d53b299ee1a419252cd1ca9
Solomon Tang <solomoncztang@gmail.com>
2021-12-08 19:53:45 -0800

    remove unused line plot

--- src/containers/dashboard/DashboardContainer.js
+++ src/containers/dashboard/DashboardContainer.js
@@ -5,14 +5,12 @@ import {
 } from 'lattice-ui-kit';
 
 import AllStudiesContainer from './AllStudiesContainer';
-import SummaryLinePlot from './components/SummaryLinePlot';
 import SummaryStatsContainer from './SummaryStatsContainer';
 
 const DashboardContainer = () => (
   <CardStack>
     <Typography variant="h1">Global Dashboard</Typography>
     <SummaryStatsContainer />
-    <SummaryLinePlot />
     <AllStudiesContainer />
   </CardStack>
 );
