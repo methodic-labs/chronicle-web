635cb8974b0a6f8a02b2fc88c9b4d80a8ed0c91b
Alfonce Nzioka <alfonce.starfish@gmail.com>
2021-12-15 03:36:56 +0300

    separate component for daily  app usage survey

---
+++ src/containers/survey/DailyAppUsageSurvey.js
@@ -0,0 +1,91 @@
+// @flow
+
+import { Map } from 'immutable';
+import {
+  AppContainerWrapper,
+  AppContentWrapper,
+  AppHeaderWrapper,
+  Box,
+  Spinner
+} from 'lattice-ui-kit';
+import { ReduxUtils } from 'lattice-utils';
+import { DateTime } from 'luxon';
+import { RequestStates } from 'redux-reqseq';
+import type { RequestState } from 'redux-reqseq';
+
+import SubmissionSuccessful from './components/SubmissionSuccessful';
+import SurveyForm from './components/SurveyForm';
+
+import BasicErrorComponent from '../shared/BasicErrorComponent';
+import OpenLatticeIcon from '../../assets/images/ol_icon.png';
+
+const { isPending } = ReduxUtils;
+
+type Props = {
+  data :Map;
+  date :string;
+  submitSurveyRS :?RequestState;
+  getUserAppsRS :?RequestState;
+  participantId :string;
+  organizationId :UUID;
+  studyId :UUID ;
+}
+
+const SurveyContainer = (props :Props) => {
+  const {
+    data,
+    date,
+    studyId,
+    organizationId,
+    participantId,
+    getUserAppsRS,
+    submitSurveyRS,
+  } = props;
+
+  if (isPending(getUserAppsRS)) {
+    return (
+      <Box mt="60px" textAlign="center">
+        <Spinner size="2x" />
+      </Box>
+    );
+  }
+
+  return (
+    <AppContainerWrapper>
+      <AppHeaderWrapper appIcon={OpenLatticeIcon} appTitle="Chronicle" />
+      <AppContentWrapper>
+        {
+          getUserAppsRS === RequestStates.FAILURE && <BasicErrorComponent />
+        }
+        {
+          getUserAppsRS === RequestStates.SUCCESS && (
+            <>
+              {
+                submitSurveyRS === RequestStates.SUCCESS
+                  ? <SubmissionSuccessful />
+                  : (
+                    <>
+                      <Box fontSize="20px" fontWeight={400}>
+                        Apps Usage Survey
+                      </Box>
+                      <Box fontSize="16px" fontWeight={400} mt="5px" mb="20px">
+                        { DateTime.fromISO(date).toLocaleString(DateTime.DATE_FULL) }
+                      </Box>
+                      <SurveyForm
+                          organizationId={organizationId}
+                          participantId={participantId}
+                          studyId={studyId}
+                          submitSurveyRS={submitSurveyRS}
+                          userAppsData={data} />
+                    </>
+                  )
+              }
+            </>
+          )
+        }
+      </AppContentWrapper>
+    </AppContainerWrapper>
+  );
+};
+
+export default SurveyContainer;
