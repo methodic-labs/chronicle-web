1344aeb2cdd710983388bfcb0c7034dea78389cf
Todd Bergman <todd@openlattice.com>
2022-01-20 13:17:27 -0800

    update error handling with worker response, since we're using axios directly.

--- src/containers/message/sagas/sendMessage.js
+++ src/containers/message/sagas/sendMessage.js
@@ -7,6 +7,7 @@ import {
 } from '@redux-saga/core/effects';
 import { Logger } from 'lattice-utils';
 import type { Saga } from '@redux-saga/core';
+import type { WorkerResponse } from 'lattice-sagas';
 import type { SequenceAction } from 'redux-reqseq';
 
 import * as ChronicleApi from '../../../utils/api/ChronicleApi';
@@ -18,22 +19,26 @@ import {
 const LOG = new Logger('MessageSagas');
 
 function* sendMessageWorker(action :SequenceAction) :Saga<*> {
+  let workerResponse :WorkerResponse;
   try {
     yield put(sendMessage.request(action.id));
     const { messageDetailsList, organizationId } = action.value;
 
     const response = yield call(ChronicleApi.sendMessage, organizationId, messageDetailsList);
-    if (response.error) throw response.error;
+    workerResponse = { data: response.data };
 
     yield put(sendMessage.success(action.id));
   }
   catch (error) {
     LOG.error(action.type, error);
+    workerResponse = { error };
     yield put(sendMessage.failure(action.id));
   }
   finally {
     yield put(sendMessage.finally(action.id));
   }
+
+  return workerResponse;
 }
 
 function* sendMessageWatcher() :Saga<*> {
