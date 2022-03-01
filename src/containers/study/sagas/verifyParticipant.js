162e281af4ec2607afbe105f2f0a1a91e97f35ec
Hristo Oskov <hristo@openlattice.com>
2022-03-01 12:00:25 -0800

    fix verifyParticipant saga

--- src/containers/study/sagas/verifyParticipant.js
+++ src/containers/study/sagas/verifyParticipant.js
@@ -9,7 +9,7 @@ import type { SequenceAction } from 'redux-reqseq';
 
 import * as StudyApi from '../../../core/api/study';
 import { ERR_INVALID_PARTICIPANT, PARTICIPANT_ID, STUDY_ID } from '../../../common/constants';
-import { REGISTER_PARTICIPANT, verifyParticipant } from '../actions';
+import { VERIFY_PARTICIPANT, verifyParticipant } from '../actions';
 import type { WorkerResponse } from '../../../common/types';
 
 const { toSagaError } = AxiosUtils;
@@ -19,17 +19,17 @@ const LOG = new Logger('StudySagas');
 function* verifyParticipantWorker(action :SequenceAction) :Saga<WorkerResponse> {
 
   let workerResponse :WorkerResponse;
-  const { id, value } = action;
+  const { id, type, value } = action;
 
   try {
     yield put(verifyParticipant.request(id, value));
-    const isValid = yield call(StudyApi.verifyParticipant, action.value[STUDY_ID], action.value[PARTICIPANT_ID]);
+    const isValid = yield call(StudyApi.verifyParticipant, value[STUDY_ID], value[PARTICIPANT_ID]);
     if (!isValid) throw Error(ERR_INVALID_PARTICIPANT);
     workerResponse = { data: isValid };
     yield put(verifyParticipant.success(id, isValid));
   }
   catch (error) {
-    LOG.error(action.type, error);
+    LOG.error(type, error);
     workerResponse = { error };
     yield put(verifyParticipant.failure(id, toSagaError(error)));
   }
@@ -42,7 +42,7 @@ function* verifyParticipantWorker(action :SequenceAction) :Saga<WorkerResponse> 
 
 function* verifyParticipantWatcher() :Saga<*> {
 
-  yield takeEvery(REGISTER_PARTICIPANT, verifyParticipantWorker);
+  yield takeEvery(VERIFY_PARTICIPANT, verifyParticipantWorker);
 }
 
 export {
