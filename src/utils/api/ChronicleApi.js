a4aa45d8d30621e34139a052ec103e96bceb9118
Hristo Oskov <hristo@openlattice.com>
2022-03-01 21:22:09 -0800

    remove unused

--- src/utils/api/ChronicleApi.js
+++ src/utils/api/ChronicleApi.js
@@ -2,11 +2,9 @@
 
 import axios from 'axios';
 import { Types } from 'lattice';
-import { DateTime } from 'luxon';
 
 import { getAuthToken } from '../../core/auth/utils';
 import {
-  getAppUsageDataUrl,
   getDeleteParticipantPath,
   getDeleteStudyUrl,
   getQuestionnaireUrl,
@@ -17,80 +15,6 @@ import {
 const { DeleteTypes } = Types;
 
 const CAFE_ORG_ID :UUID = '7349c446-2acc-4d14-b2a9-a13be39cff93';
-
-/*
- * `GET chronicle/study/participant/data/<study_id>/<participant_id>/apps`
- *
- * Fetch neighbors of participant_id associated by chroncile_user_apps.
- *s
- * response data:
-      [
-        {
-          entityDetails: {
-            FQN1: [value1],
-            FQN2: [value2]
-         },
-         associationDetails: {
-            FQN3: [value3],
-            FQN4: [value4]
-         },
-        }
-      ]
- */
-function getAppUsageSurveyData(date :string, participantId :string, studyId :UUID) {
-  return new Promise<*>((resolve, reject) => {
-    const url = getAppUsageDataUrl(participantId, studyId);
-    if (!url) return reject(new Error('Invalid Url'));
-
-    // expect date to match yyyy-MM-dd format
-    const startDate = DateTime.fromFormat(date, 'yyyy-MM-dd');
-    if (!startDate.isValid) return reject(Error(`Invalid date: ${date}`));
-
-    return axios({
-      method: 'get',
-      params: {
-        startDate: startDate.toISO(),
-        endDate: startDate.endOf('day').toISO()
-      },
-      url: encodeURI(url),
-    }).then((result) => resolve(result))
-      .catch((error) => reject(error));
-  });
-}
-
-/*
- * `POST chronicle/study/participant/data/<study_id>/<participant_id>/apps`
- *
- * Update chronicle_used_by associations with app user type (parent, child, parent_and_child)
- *
- *
- * requestBody:
-    {
-      EKID_1: {
-        FQN1: [value1],
-        FQN2: [value2]
-      },
-      EKID_1: {
-        FQN1: [value3],
-        FQN2: [value4]
-      },
-    }
- */
-
-function submitAppUsageSurvey(studyId :UUID, participantId :string, requestBody :Object) {
-  return new Promise<*>((resolve, reject) => {
-
-    const url = getAppUsageDataUrl(participantId, studyId);
-    if (!url) return reject(new Error('Invalid Url'));
-
-    return axios({
-      method: 'post',
-      data: requestBody,
-      url: encodeURI(url),
-    }).then((result) => resolve(result))
-      .catch((error) => reject(error));
-  });
-}
 
 // delete a participant and neighbors
 function deleteStudyParticipant(orgId :UUID = CAFE_ORG_ID, participantId :string, studyId :UUID) {
@@ -192,9 +116,7 @@ function getStudySettings(studyId :UUID) {
 export {
   deleteStudy,
   deleteStudyParticipant,
-  getAppUsageSurveyData,
   getQuestionnaire,
   getStudySettings,
-  submitAppUsageSurvey,
   submitQuestionnaire,
 };
