8b85b27a4d4078623334e8d2824952e9b8931ddc
Hristo Oskov <hristo.oskov@gmail.com>
2022-10-17 11:58:22 -0700

    these don't make sense

--- src/containers/tud/constants/GeneralConstants.js
+++ src/containers/tud/constants/GeneralConstants.js
@@ -19,9 +19,7 @@ const QUESTION_TITLE_LOOKUP = {
   [PROPERTY_CONSTS.BG_AUDIO_DAY]: 'Background audio',
   [PROPERTY_CONSTS.BG_TV_DAY]: 'Background TV',
   [PROPERTY_CONSTS.CAREGIVER]: 'Caregiver',
-  [PROPERTY_CONSTS.DAY_END_TIME]: 'Bedtime last night',
   [PROPERTY_CONSTS.DAY_OF_WEEK]: 'Day of week',
-  [PROPERTY_CONSTS.DAY_START_TIME]: 'Wake-up time yesterday',
   [PROPERTY_CONSTS.NON_TYPICAL_DAY_REASON]: 'Non typical day reason',
   [PROPERTY_CONSTS.NON_TYPICAL_SLEEP_PATTERN]: 'Non typical sleep reason',
   [PROPERTY_CONSTS.OTHER_ACTIVITY]: 'Other activity',
@@ -36,7 +34,6 @@ const QUESTION_TITLE_LOOKUP = {
   [PROPERTY_CONSTS.SECONDARY_MEDIA_NAME]: 'Secondary media name',
   [PROPERTY_CONSTS.SLEEP_ARRANGEMENT]: 'Sleep arrangement',
   [PROPERTY_CONSTS.SLEEP_PATTERN]: 'Sleep pattern',
-  [PROPERTY_CONSTS.TODAY_WAKEUP_TIME]: 'Wake-up time today',
   [PROPERTY_CONSTS.TYPICAL_DAY_FLAG]: 'Typical day',
   [PROPERTY_CONSTS.WAKE_UP_COUNT]: 'Wake up at night'
 };
