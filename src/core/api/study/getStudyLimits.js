841c868970338f9716e0bedc43e31e3e9eb52ba3
Hristo Oskov <UnsungHero97@users.noreply.github.com>
2023-03-27 13:16:31 -0700

    Update getStudyLimits.js

--- src/core/api/study/getStudyLimits.js
+++ src/core/api/study/getStudyLimits.js
@@ -9,7 +9,7 @@ import type { UUID } from '../../../common/types';
 export default function getStudyLimits(studyId :UUID) :Promise<*> {
 
   return getApiAxiosInstance(STUDY_LIMITS_API)
-    .get(`/${STUDY}/${STUDY}/${studyId}`)
+    .get(`/${STUDY}/${studyId}`)
     .then((axiosResponse) => axiosResponse.data)
     .catch((error :Error) => Promise.reject(error));
 }
